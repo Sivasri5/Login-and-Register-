e4b1ce42021e22052608079811edbc48dc616f02
Sivasri R <sivasrir.22cse@kongu.edu>
2025-08-04 20:10:29 +0530

    Backend link integrated

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://login-signup-z1b9.onrender.com',
+  //baseURL: 'http://localhost:5000/api',
   withCredentials: true
 });
 
