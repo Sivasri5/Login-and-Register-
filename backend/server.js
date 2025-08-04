64db3040b876ac0dc4bfffa1514ef009b7b08a70
Sivasri R <sivasrir.22cse@kongu.edu>
2025-08-04 20:26:42 +0530

    frontend link added

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://login-signup-0bpi.onrender.com',
+  //origin: 'http://localhost:3000',
   credentials: true,
 }));
 
